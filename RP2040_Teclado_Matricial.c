be619025f30b888e855cb5f08d7fe3d925af8ebf
HUGO S. DIAS <39714844+hsantosdias@users.noreply.github.com>
2025-01-27 19:30:29 -0300

    Atualização de variaveis!

--- RP2040_Teclado_Matricial.c
+++ RP2040_Teclado_Matricial.c
@@ -3,15 +3,15 @@
 #include "MatrixKeypad.h"
 
 // Configuração do teclado matricial
-#define ROWS 4
-#define COLS 4
+#define ROWSPIN 4
+#define COLSPIN 4
 
 // Definição dos pinos GPIO conectados às linhas e colunas do teclado
-uint8_t rowPins[ROWS] = {18, 19, 20, 4};  // GPIOs para as linhas
-uint8_t colPins[COLS] ={16, 17, 9, 8};  // GPIOs para as colunas
+uint8_t rowPins[ROWSPIN] = {18, 19, 20, 4};  // GPIOs para as linhas
+uint8_t colPins[COLSPIN] ={16, 17, 9, 8};  // GPIOs para as colunas
 
 // Mapeamento das teclas (4x4)
-char keyMap[ROWS][COLS] = {
+char keyMap[ROWSPIN][COLSPIN] = {
     {'1', '2', '3', 'A'},
     {'4', '5', '6', 'B'},
     {'7', '8', '9', 'C'},
@@ -23,7 +23,7 @@ int main() {
     stdio_init_all();
 
     // Criação do objeto MatrixKeypad
-    MatrixKeypad_t *keypad = MatrixKeypad_create((char *)keyMap, rowPins, colPins, ROWS, COLS);
+    MatrixKeypad_t *keypad = MatrixKeypad_create((char *)keyMap, rowPins, colPins, ROWSPIN, COLSPIN);
     if (keypad == NULL) {
         printf("Erro: Falha ao inicializar o teclado matricial!\n");
         return 1;
